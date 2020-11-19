a4921558535adc96a1ef644a9d4d7f56d1028a06
satriaherman <45140436+satriaherman1@users.noreply.github.com>
2020-11-19 10:18:57 +0700

    mengubah server database
    mengubah server database

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'votin.database.windows.net',
+	'hostname' => 'tcp:votin.database.windows.net',
 	'username' => 'satria@votin',
 	'password' => 'Hertsensei08;',
 	'database' => 'leadership',

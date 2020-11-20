89dbc42cf012f04652fb59862d262e3ee4cf53c4
satriaherman <45140436+satriaherman1@users.noreply.github.com>
2020-11-20 15:26:01 +0700

    update style in view

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -20,7 +20,7 @@
         <div class="content-container">
            
             <?php foreach($ketua_1 as $daftar): ?>
-            <div class="card   border-0 shadow-sm"  data-aos="fade-left">
+            <div class="card   border-0 shadow-sm" style=" width:100%; "  data-aos="fade-left">
             <div bg-img="<?= base_url();?>/assets/resources/kandidat/<?= $daftar['foto'];?>" class="card-img-top img" > </div>
             <div class="card-body">
                 <h5 class="card-title"><?= $daftar['nama']; ?></h5>
